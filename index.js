3390ebe8b5727c7e6c89a000c2bdd0c5bedba4ff
Brandon Sweetman <bclean92@gmail.com>
2018-01-13 16:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Brandon"
-var height = "70"
+var height = 70
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) +1)
+console.log(height +1)
 
